7f9dc022cc3ad686961dba5842faa98d6af92074
Ruan <rugcosta1234@gmail.com>
2024-09-06 08:18:01 -0300

    Adicionando o texto de email da tela de login

--- src/pages/TelaLogin.js
+++ src/pages/TelaLogin.js
@@ -1,19 +1,21 @@
 import React from "react"; //Importando a biblioteca React
-import { View, Text, StyleSheet, Image } from "react-native"; //Importando os elementos da biblioteca react native 
-
+import { View, Text, StyleSheet, Image, TextInput } from "react-native"; //Importando os elementos da biblioteca react native
 
 //Componente tela de login
 export default function TelaLogin() {
   return (
     //Tag para uma caixa para colocar elementos
     <View style={styles.container}>
-        {/* Logo da tela de login */}
-      <Image 
-      source={require('../../assets/images/tela-login.png')} 
-      style={styles.logo}
+      {/* Logo da tela de login */}
+      <Image
+        source={require("../../assets/images/tela-login.png")}
+        style={styles.logo}
       />
-      {/* Tag para adicionar text */}
       <Text style={styles.text}>ArmaTech</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.emailText}>Email</Text>
+        
+      </View>
     </View>
   );
 }
@@ -22,8 +24,8 @@ export default function TelaLogin() {
 const styles = StyleSheet.create({
   container: {
     flex: 1, // Garante que o container preencha toda a tela
-    alignItems: "center",
- 
+    width: "100%",
+
     // Style da imagel da tela de login
   },
   logo: {
@@ -31,16 +33,32 @@ const styles = StyleSheet.create({
     height: 240,
     marginTop: 50,
     marginBottom: 15,
-
+    alignSelf: "center",
   },
 
   //Style do titulo da tela de login
   text: {
     fontSize: 45,
-    color: '#fafafa',
-    fontFamily: 'Inter',
-    fontWeight: '700',
+    color: "#fafafa",
+    fontFamily: "Inter",
+    fontWeight: "700",
     lineHeight: 42,
     marginTop: 0,
-  }
+    alignSelf: "center",
+  },
+
+  textContainer: {
+    width: "100%",
+    paddingHorizontal: 18,
+  },
+
+  emailText: {
+    textAlign: "left",
+    marginRight: 5,
+    fontSize: 20,
+    color: "white",
+    fontFamily: "Roboto",
+    fontWeight: "700",
+    marginTop: 50,
+  },
 });
