ae516dbc9b7b0145c69e8d70a43a8a515c80fa5f
Ruan <rugcosta1234@gmail.com>
2024-09-06 00:17:16 -0300

    Adicinando a logo e o tiitulo na tela de login

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: '#283949',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/pages/TelaLogin.js
+++ src/pages/TelaLogin.js
@@ -1,26 +1,43 @@
-import React from "react";//Importando a biblioteca React
-import { View, Text, StyleSheet } from "react-native";//Importando a biblioteca React
+import React from "react"; //Importando a biblioteca React
+import { View, Text, StyleSheet, Image } from "react-native"; //Importando a biblioteca React
 
 //
-export default function TelaLogin(){
-    return(
-        //Tag para uma caixa para colocar elementos
-        <View style={styles.container}>
-            {/* Tag para adicionar text */}
-            <Text>
-            Tela de login
-            </Text>
-        </View>
-    )
+export default function TelaLogin() {
+  return (
+    //Tag para uma caixa para colocar elementos
+    <View style={styles.container}>
+      <Image 
+      source={require('../../assets/images/tela-login.png')} 
+      style={styles.logo}
+      />
+      {/* Tag para adicionar text */}
+      <Text style={styles.text}>ArmaTech</Text>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1, // Garante que o container preencha toda a tela
-      backgroundColor: 'orange', // Define a cor de fundo
-      alignItems: 'center',
-    justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1, // Garante que o container preencha toda a tela
+    alignItems: "center",
+ 
+  },
+  logo: {
+    width: 240,
+    height: 240,
+    marginTop: 50,
+    marginBottom: 15,
+
+  },
+
+  text: {
+    fontSize: 45,
+    color: '#fafafa',
+    fontFamily: 'Inter',
+    fontWeight: '700',
+    lineHeight: 42,
+    marginTop: 0,
   
+    
+  }
+});
