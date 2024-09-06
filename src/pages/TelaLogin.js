ea76522c935dc2ad86e746ebcd65b8cf6b0f5521
Ruan <rugcosta1234@gmail.com>
2024-09-06 00:20:17 -0300

    Adicionando comentarios

--- src/pages/TelaLogin.js
+++ src/pages/TelaLogin.js
@@ -1,11 +1,13 @@
 import React from "react"; //Importando a biblioteca React
-import { View, Text, StyleSheet, Image } from "react-native"; //Importando a biblioteca React
+import { View, Text, StyleSheet, Image } from "react-native"; //Importando os elementos da biblioteca react native 
 
-//
+
+//Componente tela de login
 export default function TelaLogin() {
   return (
     //Tag para uma caixa para colocar elementos
     <View style={styles.container}>
+        {/* Logo da tela de login */}
       <Image 
       source={require('../../assets/images/tela-login.png')} 
       style={styles.logo}
@@ -16,11 +18,13 @@ export default function TelaLogin() {
   );
 }
 
+//Style da tela de login
 const styles = StyleSheet.create({
   container: {
     flex: 1, // Garante que o container preencha toda a tela
     alignItems: "center",
  
+    // Style da imagel da tela de login
   },
   logo: {
     width: 240,
@@ -30,6 +34,7 @@ const styles = StyleSheet.create({
 
   },
 
+  //Style do titulo da tela de login
   text: {
     fontSize: 45,
     color: '#fafafa',
@@ -37,7 +42,5 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     lineHeight: 42,
     marginTop: 0,
-  
-    
   }
 });
