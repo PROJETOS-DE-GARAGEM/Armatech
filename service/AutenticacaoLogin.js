19dc8f9fbdb9351f29878062c6f1e4107140bd3f
unknown <rugcosta1234@gmail.com>
2024-09-11 11:30:14 -0300

    adicioanndo a logica de autenticação de login

--- service/AutenticacaoLogin.js
+++ service/AutenticacaoLogin.js
@@ -1,16 +1,26 @@
 import axios from "axios";
 
-const API_URL = "http://192.168.1.5:3000";
+const API_URL = "http://192.168.18.14:3000";
 console.log(API_URL);
 
 export const autenticar = async (email, senha) => {
   try {
-    const response = await axios.get(
-      `${API_URL}/users?email=${email}&senha=${senha}`,
-      { email, senha }
-    );
-    return response.data;
+    // Passa os parâmetros usando a opção `params`
+    const response = await axios.get(`${API_URL}/users`, {
+      params: { email, senha },
+    });
+    
+    // Verifica a estrutura da resposta
+    console.log("Resposta da API:", response.data);
+    
+    if (response.data.length > 0) {
+      return response.data[0]; // Retorna o primeiro usuário encontrado
+    } else {
+      throw new Error("Usuário não encontrado");
+    }
   } catch (error) {
+    console.error("Erro de autenticação:", error);
     throw new Error("Falha na autenticação");
   }
 };
+
