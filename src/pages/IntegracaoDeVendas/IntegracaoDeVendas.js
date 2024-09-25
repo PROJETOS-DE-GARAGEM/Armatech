9297afc8680b7d341c1ca01164e8660bbcb2af97
unknown <rugcosta1234@gmail.com>
2024-09-25 14:51:33 -0300

    Adicionando a page de vendas status: Em andamento

--- src/pages/IntegracaoDeVendas/IntegracaoDeVendas.js
+++ src/pages/IntegracaoDeVendas/IntegracaoDeVendas.js
@@ -1,15 +1,56 @@
-import React from "react";
-import { View } from "react-native";
-import style from "./IntegracaoDeVendasStyle";
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import styles from "./IntegracaoDeVendasStyle";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import { Dropdown } from "react-native-element-dropdown";
+import AntDesign from 'react-native-vector-icons/AntDesign'; // Import correto
 
-export default function IntegracaoDeVendas({navigation}){
-    return (
-        <View style={style.container}>
-        <Header titulo="Integração de Vendas" navigation={navigation} />
-        <View></View>
-        <Footer />
+export default function IntegracaoDeVendas({ navigation }) {
+  const [value, setValue] = useState(null);
+
+  // Dados para o dropdown
+  const data = [
+    { label: 'Produto 1', value: '1' },
+    { label: 'Produto 2', value: '2' },
+    { label: 'Produto 3', value: '3' },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <Header titulo="Integração de Vendas" navigation={navigation} />
+      
+      {/* Titulo */}
+      <View>
+        <Text style={styles.title}>Nova Venda</Text>
       </View>
-    );
+      
+      <View>
+        <Text style={styles.label}>Nome do Produto</Text>
+      </View>
+
+      {/* Dropdown */}
+      <Dropdown
+        style={styles.dropdown}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data} // Definindo a variável de dados
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder="Selecione um item"
+        searchPlaceholder="Buscar..."
+        value={value}
+        onChange={(item) => {
+          setValue(item.value);
+        }}
+        
+      />
+
+      <Footer />
+    </View>
+  );
 }
--- src/pages/IntegracaoDeVendas/IntegracaoDeVendasStyle.js
+++ src/pages/IntegracaoDeVendas/IntegracaoDeVendasStyle.js
@@ -1,10 +1,31 @@
 import { StyleSheet } from "react-native";
 
-const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#283949",
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#283949",
+  },
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 5
+  },
+ 
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  iconStyle: {
+    paddingHorizontal: 6
+  }
+});
 
-export default style;
+export default styles;
