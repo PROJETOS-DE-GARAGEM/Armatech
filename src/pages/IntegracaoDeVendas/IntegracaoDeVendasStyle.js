6a346af1c55ca3e0761b8c3c1b8dc580589d3ec0
Ruan <rugcosta1234@gmail.com>
2024-09-26 08:06:49 -0300

    Foi feito algumas alterações no estilo na pagina de vendas

--- src/pages/IntegracaoDeVendas/IntegracaoDeVendasStyle.js
+++ src/pages/IntegracaoDeVendas/IntegracaoDeVendasStyle.js
@@ -5,16 +5,41 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#283949",
   },
+  title: {
+    paddingTop: "10%",
+    marginLeft: "5%",
+    color: "#fff",
+    fontSize: 23,
+    fontWeight: "bold",
+    flexDirection: "row",
+  },
+
+  text: {
+    paddingTop: "5%",
+    marginLeft: "5%",
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+    flexDirection: "row",
+  },
   dropdown: {
     margin: 16,
     height: 50,
-    borderColor: "white",
-    borderWidth: 1,
-    borderRadius: 5
+    color: "#000",
+    fontSize: 17,
+    height: 40,
+    borderBottomWidth: 1,
+    borderColor: "#fff",
+    marginStart: "5%",
+    marginEnd: "5%",
+    backgroundColor: "#fff",
+    borderRadius: 999,
+    paddingHorizontal: 9,
   },
- 
+
   placeholderStyle: {
     fontSize: 16,
+    color: "gray",
   },
   selectedTextStyle: {
     fontSize: 16,
@@ -22,10 +47,25 @@ const styles = StyleSheet.create({
   inputSearchStyle: {
     height: 40,
     fontSize: 16,
+    borderRadius: 10,
   },
   iconStyle: {
-    paddingHorizontal: 6
-  }
+    paddingHorizontal: 6,
+  },
+  dropDownContainerStyle: {
+    backgroundColor: "#fff", // Cor de fundo da lista
+    borderRadius: 15, // Borda arredondada do dropdown expandido
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  textDescricao: {
+    paddingTop: "4%",
+    marginLeft: "5%",
+    color: "#fff",
+    fontSize: 17,
+    fontWeight: "bold",
+    flexDirection: "row",
+  },
 });
 
 export default styles;
